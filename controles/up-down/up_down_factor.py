62c4d28b4bf67f37577fa29e2b7299351df081b7
Bruno Torres <63613472+bruno-ba@users.noreply.github.com>
2020-04-17 08:34:34 -0300

    bloqueio de botões
    Complemento de código que bloqueia os botões enquanto está escolhendo os fatores, evitando-se assim o acionamento enquandonão decidiu o verdadeiro valor

--- controles/up-down/up_down_factor.py
+++ controles/up-down/up_down_factor.py
@@ -1,7 +1,7 @@
 import kivy
 import re
 from kivy.app import App
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, BooleanProperty
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
@@ -21,33 +21,33 @@ class UpDown(Widget):
     current_value = NumericProperty(0)  # valor atual do contador
     up_factor = NumericProperty(1)  # fator de subida
     down_factor = NumericProperty(-1)  # fator de descida
+    up_lock = BooleanProperty(False)  # aciona o bloqueio do botão up
+    down_lock = BooleanProperty(False)  # aciona o bloqueio do botão down
 
     def set_factor(self, value: str, direction: bool) -> None:  # configura valores para os fatores up e down
+
         if direction:
+            self.up_lock = not self.up_lock
             up_mo = UpDown.up_factor_regex.search(value)
+            self.up_factor = 0
 
             if up_mo:
-                print(up_mo.group())
                 uf = int(up_mo.group())
-                self.up_factor = 0
-                self.up_factor = uf
-
+                self.up_factor = 1 if uf <= 0 else uf
 
             else:
-                self.up_factor = 0
                 self.up_factor = 1
 
         else:
+            self.down_lock = not self.down_lock
             down_mo = UpDown.down_factor_regex.search(value)
+            self.down_factor = 0
 
             if down_mo:
-                print(down_mo.group())
                 df = int(down_mo.group())
-                self.down_factor = 0
                 self.down_factor = df if df < 0 else df * -1
 
             else:
-                self.down_factor = 0
                 self.down_factor = -1
 
     def increment_current_value(self, direction: bool) -> None:  # incrementa ou decrementa o valor corrente
@@ -61,9 +61,13 @@ code = """
     command_txt: None
     command_arg : None
     factor: None
+    disabled_btn: True
+    
     Button:
         text: str(root.factor)
         on_press: root.command_btn(root.command_arg)
+        disabled: root.disabled_btn
+        
     TextInput:  
         size_hint_y: .5
         text: str(root.factor)
@@ -90,12 +94,14 @@ code = """
                 command_txt: root.set_factor
                 command_arg: False
                 factor: root.down_factor
+                disabled_btn: root.down_lock
                 
             FactorButton:
                 command_btn: root.increment_current_value
                 command_txt: root.set_factor
                 command_arg: True
                 factor: root.up_factor
+                disabled_btn: root.up_lock
                 
 UpDown:
 """
